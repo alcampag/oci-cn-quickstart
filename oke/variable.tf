6ec03f204d051cedee09787419b4b6d7b505f33b
Alberto Campagna <alberto.campagna@oracle.com>
2024-07-12 12:28:47 +0200

    added oke quickstart solution

--- oke/variable.tf
+++ oke/variable.tf
@@ -2,10 +2,10 @@ variable "tenancy_ocid" {}
 variable "region" {}
 variable "compartment_ocid" {}
 variable "oke_image_name" {
-  default = "Oracle-Linux-7.9-2024.05.29-0-OKE-1.29.1-707"
+  default = "Oracle-Linux-8.9-2024.05.29-0-OKE-1.29.1-707"
 }
 variable "cni_type" {
-  default = "npn"
+  default = "npn"   # flannel if you need an OKE cluster with Flannel
 }
 
 variable "kubernetes_version" {
