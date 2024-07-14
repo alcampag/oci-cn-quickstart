533b784ccf0363cda44727bbd27e0edc990dc6dd
Alberto Campagna <alberto.campagna@oracle.com>
2024-07-14 13:12:22 +0200

    oci-native solution development

--- oci-native/modules/network/lb-nsg.tf
+++ oci-native/modules/network/lb-nsg.tf
@@ -35,3 +35,71 @@ resource "oci_core_network_security_group_security_rule" "oke_lb_nsg_rule_worker
     }
   }
 }
+
+resource "oci_core_network_security_group_security_rule" "oke_lb_nsg_rule_workers_healthcheck_egress" {
+  direction                 = "EGRESS"
+  network_security_group_id = oci_core_network_security_group.oke_lb_nsg
+  protocol                  = "6"
+  destination_type = "NETWORK_SECURITY_GROUP"
+  destination = module.oke-network.worker_nsg_id
+  stateless = false
+  description = "Allow TCP egress from load balancers to worker nodes for health check"
+  tcp_options {
+    destination_port_range {
+      max = 10256
+      min = 10256
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "oke_lb_nsg_rule_https_ingress" {
+  direction                 = "INGRESS"
+  network_security_group_id = oci_core_network_security_group.oke_lb_nsg
+  protocol                  = "6"
+  source_type = "CIDR_BLOCK"
+  source = "0.0.0.0/0"
+  stateless = true
+  description = "Allow https traffic - stateless Ingress"
+  tcp_options {
+    destination_port_range {
+      max = 443
+      min = 443
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "oke_lb_nsg_rule_https_egress" {
+  direction                 = "EGRESS"
+  network_security_group_id = oci_core_network_security_group.oke_lb_nsg
+  protocol                  = "6"
+  destination_type = "CIDR_BLOCK"
+  destination = "0.0.0.0/0"
+  stateless = true
+  description = "Allow https traffic - stateless egress"
+  tcp_options {
+    source_port_range {
+      max = 443
+      min = 443
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "oke_lb_nsg_rule_pods_egress" {
+  direction                 = "EGRESS"
+  network_security_group_id = oci_core_network_security_group.oke_lb_nsg
+  protocol                  = "6"
+  destination_type = "NETWORK_SECURITY_GROUP"
+  destination = module.oke-network.pod_nsg_id
+  stateless = true
+  description = "LB to pods, OCI Native Ingress - stateless egress"
+}
+
+resource "oci_core_network_security_group_security_rule" "oke_lb_nsg_rule_pods_ingress" {
+  direction                 = "INGRESS"
+  network_security_group_id = oci_core_network_security_group.oke_lb_nsg
+  protocol                  = "6"
+  source_type = "NETWORK_SECURITY_GROUP"
+  source = module.oke-network.pod_nsg_id
+  stateless = true
+  description = "LB to pods, OCI Native Ingress - stateless ingress"
+}
