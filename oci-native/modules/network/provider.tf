e6b4f8d2074e3716e4b8a62f41d7da8ca6c6da8f
alcampag <comes94@gmail.com>
2024-07-25 18:12:17 +0200

    refactored

--- oci-native/modules/network/provider.tf
+++ oci-native/modules/network/provider.tf
@@ -1,17 +1,8 @@
 terraform {
-  required_version = ">=1.5.0"
   required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = ">=6.3.0"
-    }
     null = {
       source = "hashicorp/null"
       version = "3.2.2"
     }
   }
-}
-
-provider "oci" {
-  region = var.region
 }
