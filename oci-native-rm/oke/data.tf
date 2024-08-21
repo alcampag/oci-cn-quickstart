790f73d7cb31733e99f17f943b85ddb093c53b72
alcampag <comes94@gmail.com>
2024-08-21 19:17:27 +0200

    oke for rm

---
+++ oci-native-rm/oke/data.tf
@@ -0,0 +1,7 @@
+data "oci_identity_region_subscriptions" "home" {
+  tenancy_id = var.tenancy_ocid
+  filter {
+    name   = "is_home_region"
+    values = [true]
+  }
+}
---
+++ oci-native-rm/oke/variable.tf
@@ -0,0 +1,25 @@
+variable "tenancy_ocid" {}
+variable "my_region" {}
+
+# NETWORK
+
+variable "network_compartment_id" {}
+variable "vcn_id" {}
+variable "lb_subnet_id" {}
+variable "cp_subnet_id" {}
+variable "worker_subnet_id" {}
+variable "pod_subnet_id" {}
+variable "cp_nsg_id" {}
+variable "cp_allowed_cidr_list" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+
+# CLUSTER
+
+variable "oke_compartment_id" {}
+variable "cluster_name" {
+  default = "oke-rm-quickstart"
+}
+variable "kubernetes_version" {}
