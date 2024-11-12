8b71b5533333867ddd4611283e43ac91f26d1f71
alcampag <alberto.campagna@oracle.com>
2024-11-12 11:55:30 +0100

    support for subnet creation option

--- oci-native-rm/infra/modules/network/local.tf
+++ oci-native-rm/infra/modules/network/local.tf
@@ -1,6 +1,6 @@
 locals {
   is_npn = var.cni_type == "npn"
-  create_pod_subnet = var.create_pod_subnet && ! local.is_npn
+  create_pod_subnet = var.create_pod_subnet && local.is_npn
   create_cp_subnet = var.create_cp_subnet
   create_worker_subnet = var.create_worker_subnet
   create_service_subnet = var.create_service_subnet
