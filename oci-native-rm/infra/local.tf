107c50cd275b8f60887c733ee9aa5c4ac3440d86
alcampag <alberto.campagna@oracle.com>
2025-01-22 09:19:58 +0100

    fix locals

--- oci-native-rm/infra/local.tf
+++ oci-native-rm/infra/local.tf
@@ -1,6 +1,3 @@
 locals {
-  create_certificates = var.create_certificates && var.create_vault
-  create_lb = var.create_lb && var.create_service_subnet
-  create_apigw = var.create_apigw && var.create_service_subnet
   create_bastion = var.create_bastion_subnet && var.create_bastion
 }
